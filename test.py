541b52c0783e104d6a4c5c286d8b937acb579a27
Sarika Devarajan <sarikadevarajan353@gmail.com>
2026-05-22 21:27:14 +0530

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyDhhsJ3eEYr-p44xmTs53MmNvfOW-Wv2tA")
-
-print("Listing models...")
-
-for model in genai.list_models():
-    print(model.name)
