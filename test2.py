3e975e1bf3e8bc0b894fdee13db7fc0bde977a65
Sarika Devarajan <sarikadevarajan353@gmail.com>
2026-05-22 21:27:23 +0530

    Delete test2.py

--- test2.py
+++
@@ -1,9 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyDhhsJ3eEYr-p44xmTs53MmNvfOW-Wv2tA")
-
-model = genai.GenerativeModel("gemini-2.5-flash")
-
-response = model.generate_content("Say hello")
-
-print(response.text)
